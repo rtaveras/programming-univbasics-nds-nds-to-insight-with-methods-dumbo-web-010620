c521e24b725779c200affafc48359c6789fff367
Riaan Taveras <riaantaveras@gmail.com>
2020-01-02 21:11:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,6 +14,7 @@ def directors_totals(nds)
     director_name = nds[i][:name]
     director_totals[director_name] = 0
     while x < nds[i][:movies].length
+    binding.pry
       director_totals[director_name] += gross_for_director
       x += 1 
     end
