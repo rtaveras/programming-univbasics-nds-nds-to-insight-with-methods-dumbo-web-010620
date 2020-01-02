e695619b6fb7eff2012e373d4f33059d8c09c3b4
Riaan Taveras <riaantaveras@gmail.com>
2020-01-02 21:05:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,6 @@ def gross_for_director(director_data)
   index = 0
   total = 0 
   while index < director_data[:movies].length do
-    binding.pry 
       total += director_data[:movies][index][:worldwide_gross]
       index += 1 
     end
