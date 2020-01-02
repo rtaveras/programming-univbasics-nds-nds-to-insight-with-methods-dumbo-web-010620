47e47aaa5db3fc65b5750bc9f04cdaa7ad425f1c
Riaan Taveras <riaantaveras@gmail.com>
2020-01-02 21:07:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,7 +14,7 @@ def directors_totals(nds)
     director_name = nds[i][:name]
     director_totals[director_name] = 0
     while x < nds[i][:movies].length
-      director_totals[director_name] += 
+      director_totals[director_name] += gross_for_director
       x += 1 
     end
    i += 1 
